ef1dfdc8975097d4316e4d3cc9310026b69ec208
peter <peter@retep.org>
2023-06-08 21:31:36 +0100

    Remove unit from UV & Strive Units

--- weather/measurement/units.go
+++ weather/measurement/units.go
@@ -6,8 +6,8 @@ import (
 
 func init() {
 	// Miscellaneous units which are not part of a family
-	UV = value.NewLowerBoundUnit("UV", "UV", " UV index", 0, 0)
-	Strike = value.NewLowerBoundUnit("Strikes", "Lightning Strikes", " strikes", 0, 0)
+	UV = value.NewLowerBoundUnit("UV", "UV", "", 0, 0)
+	Strike = value.NewLowerBoundUnit("Strikes", "Lightning Strikes", "", 0, 0)
 }
 
 var (
