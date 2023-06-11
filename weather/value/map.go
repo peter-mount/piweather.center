c5588b778130e79167a342a821b9298573412ce4
peter <peter@retep.org>
2023-06-11 11:33:20 +0100

    Add mutex as we can hit concurrent access to the map

--- weather/value/map.go
+++ weather/value/map.go
@@ -3,10 +3,12 @@ package value
 import (
 	"context"
 	"strings"
+	"sync"
 )
 
 type Map struct {
-	m map[string]Value
+	mutex sync.Mutex
+	m     map[string]Value
 }
 
 func WithMap(ctx context.Context) context.Context {
@@ -20,17 +22,27 @@ func MapFromContext(ctx context.Context) *Map {
 }
 
 func (m *Map) Get(k string) Value {
+	m.mutex.Lock()
+	defer m.mutex.Unlock()
+	return m.get(k)
+}
+
+func (m *Map) get(k string) Value {
 	return m.m[strings.ToLower(k)]
 }
 
 func (m *Map) GetAll(keys ...string) []Value {
+	m.mutex.Lock()
+	defer m.mutex.Unlock()
 	var args []Value
 	for _, key := range keys {
-		args = append(args, m.Get(key))
+		args = append(args, m.get(key))
 	}
 	return args
 }
 func (m *Map) Put(k string, v Value) {
+	m.mutex.Lock()
+	defer m.mutex.Unlock()
 	m.m[strings.ToLower(k)] = v
 }
 
