505a00f47f39a0982d7c1737fff8588c308bfca1
peter <peter@retep.org>
2023-11-17 09:13:48 +0000

    Allow to, to be a duration after from as well as a time.

--- store/server/request.go
+++ store/server/request.go
@@ -22,13 +22,35 @@ type Request struct {
 // GetRequest returns a Request populated with values from the inbound request
 func GetRequest(r *rest.Rest) Request {
 	query := r.Request().URL.Query()
-	return Request{
+
+	to := query.Get(TO)
+
+	req := Request{
 		Metric: strings.ReplaceAll(r.Var(METRIC), "/", "."),
 		At:     unit.ParseTime(query.Get(AT)).Truncate(time.Second),
 		From:   unit.ParseTime(query.Get(FROM)).Truncate(time.Second),
-		To:     unit.ParseTime(query.Get(TO)).Truncate(time.Second),
+		To:     unit.ParseTime(to).Truncate(time.Second),
 		Filter: query.Get(FILTER),
 	}
+
+	if !req.From.IsZero() {
+		// To provided but not a valid time, try parsing it as a duration
+		if to != "" && req.To.IsZero() {
+			d, err := time.ParseDuration(to)
+			if err == nil {
+				req.To = req.From.Add(d)
+			}
+		}
+
+		// Ensure from.Before(to)
+		// Can happen if client sets them wrong, but also if to was a
+		// negative duration
+		if !req.To.IsZero() && req.To.Before(req.From) {
+			req.From, req.To = req.To, req.From
+		}
+	}
+
+	return req
 }
 
 // Response returns an api.Response prepopulated with values from the Request
