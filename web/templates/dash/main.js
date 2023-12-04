bfc0f33e84bf84341ab1999e2adf72b1760cf95f
peter <peter@retep.org>
2023-12-04 20:56:01 +0000

    Fix issue with < being escaped

--- web/templates/dash/main.js
+++ web/templates/dash/main.js
@@ -2,18 +2,20 @@ window.addEventListener("load", wsListener)
 
 function wsListener(evt) {
     let pt = location.port,
-        url=(location.protocol === "http:" ? "ws" : "wss") + "://" + location.host + "/live/dash/{{$.dash}}",
+        url = (location.protocol === "http:" ? "ws" : "wss") + "://" + location.host + "/live/dash/{{$.dash}}",
         dashUid = "{{$.board.Uid}}",
         ws = new WebSocket(url);
 
-    ws.onopen = function (evt) {}
+    ws.onopen = function (evt) {
+    }
 
     ws.onclose = function (evt) {
         ws = null;
         setTimeout(wsListener, 2000)
     }
 
-    ws.onerror = function (evt) {}
+    ws.onerror = function (evt) {
+    }
 
     ws.onmessage = function (evt) {
         let msg = JSON.parse(evt.data), acts = msg.actions
@@ -40,12 +42,13 @@ function wsListener(evt) {
 function setRotate(id, i, ang) {
     let e = document.getElementById(id + ".ptr" + i)
     if (e !== null) {
-        let from=e.getAttribute("to"),
-            d=Math.abs(from-ang)>180,
-            td=d && from<ang,
-            fd=d && from>ang
-        e.setAttribute("from",from-(fd?360:0))
-        e.setAttribute("to",ang-(td?360:0))
+        ang = ang + (0 > ang ? 360 : ang >= 360 ? -360 : 0)
+        let from = e.getAttribute("to"),
+            d = Math.abs(from - ang) > 180,
+            td = d && ang > from,
+            fd = d && from > ang
+        e.setAttribute("from", from - (fd ? 360 : 0))
+        e.setAttribute("to", ang - (td ? 360 : 0))
         e.beginElement()
     }
 }
