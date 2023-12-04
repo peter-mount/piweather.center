583f7bd01a2d2f3502ed1d86aada1d11e59c3b7e
peter <peter@retep.org>
2023-12-04 10:28:08 +0000

    Enable live updates for rain-gauge

--- web/templates/dash/rain-gauge.js
+++ web/templates/dash/rain-gauge.js
@@ -1,5 +1,19 @@
 function update_rain_gauge(m, id, idx) {
     idx.forEach(i => {
-        //setText(act.id, i, m.formatted)
+        let d = document.getElementById(id + ".svg"),
+            min = d.dataset.min,
+            max = d.dataset.max,
+            scale = d.dataset.scale,
+            height=d.dataset.height,
+            v=m.value,
+            y=scale*(v-min)
+        if(v>max) {
+            // Update means we exceed the axis so reload to get a new axis
+            location.reload()
+            return
+        }
+        let e = document.getElementById(id + ".rect")
+        e.setAttribute("y",height-y)
+        e.setAttribute("height",y)
     })
 }
