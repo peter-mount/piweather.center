7f5273d93ff7b9161f1849d907dfd28c17cb5b00
peter <peter@retep.org>
2024-02-09 15:12:52 +0000

    Fix tests by building vsop87b before running them

--- tools/build/vsop87.go
+++ tools/build/vsop87.go
@@ -3,6 +3,7 @@ package build
 import (
 	"github.com/peter-mount/go-build/core"
 	"github.com/peter-mount/go-build/util/arch"
+	"github.com/peter-mount/go-build/util/makefile"
 	"github.com/peter-mount/go-build/util/makefile/target"
 	"github.com/peter-mount/go-build/util/meta"
 	"path/filepath"
@@ -20,6 +21,7 @@ type Vsop87Encoder struct {
 
 func (s *Vsop87Encoder) Start() error {
 	s.Build.AddExtension(s.extension)
+	s.Build.Makefile(0, s.makefile)
 	return nil
 }
 
@@ -37,4 +39,15 @@ func (s *Vsop87Encoder) extension(arch arch.Arch, target target.Builder, meta *m
 			Echo("VSOP87", dest).
 			BuildTool("-gunzip", src, "-d", dest)
 	}
+}
+
+func (s *Vsop87Encoder) makefile(root makefile.Builder, _ target.Builder, _ *meta.Meta) {
+	// This adds a dependency to test for vsop87.
+	//
+	// With gnu-make, this works because it merges this test rule with the normal one
+	// so when test is run, vsop87 is performed first so that the tests have access
+	// to that data
+	root.Rule("test",
+		filepath.Join(s.Build.BuildArch().BaseDir(*s.Encoder.Dest), "lib", "vsop87b"),
+	)
 }
