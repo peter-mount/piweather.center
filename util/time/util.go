c64a6531199e41facd8c6ee3fe91ad88da26197a
peter <peter@retep.org>
2024-05-14 15:43:18 +0100

    Pushing changes to see if system timezone affects the results

--- util/time/util.go
+++ util/time/util.go
@@ -49,19 +49,72 @@ func Zone(t time.Time) string {
 // Note: Local Midnight is usually 00:00:00 but for some TimeZones, on the day they switch to Daylight Savings
 // the do so at midnight, so this returns 01:00:00 for those TimeZones for that specific day.
 func LocalMidnight(t time.Time) time.Time {
-	// Truncate to the current hour, then subtract the remaining hours.
-	// Do not truncate to 24*time.Hour here as not all days are 24 hours long!
-	midnight := t.Truncate(time.Hour)
-	midnight = midnight.Add(-time.Duration(midnight.Hour()) * time.Hour)
+	// Reduce the time to midnight by removing the time component via subtraction.
+	// Truncate(time.Hour) works for most time zones, except for those with fractional
+	// hours in their zone offsets which causes the minutes not to be 0.
+	//
+	// Also DO NOT Truncate(24*time.Hour) here as not all days are 24 hours long!
+	if !t.IsZero() {
+		// A loop is necessary for some Australian Time Zones for some weird reason
+		// where on the first subtraction when they switch from DST to ST they still
+		// have some minutes remaining
+		//for i := 0; i < 2 && !IsMidnight(t); i++ {
+		t = t.Add(-(time.Duration(t.Hour()) * time.Hour)).
+			Add(-(time.Duration(t.Minute()) * time.Minute)).
+			Add(-(time.Duration(t.Second()) * time.Second))
 
-	// If hour is still not zero then we have a Standard/Day-Light-Saving change
-	// on this day so adjust the time accordingly, so if 1 then -1hour, 23 then add 1 hour
-	if h := midnight.Hour(); h != 0 {
-		if h >= 12 {
-			h = h - 24
+		// If hour is still not zero then we have a Standard/Day-Light-Saving change
+		// on this day so adjust the time accordingly, so if 1 then -1hour, 23 then add 1 hour
+		/*if h := t.Hour(); h >= 12 {
+			loc := t.Location()
+			tz := t.UTC().Add(time.Hour)
+			t = tz.In(loc)
+			fmt.Printf("tz %s t %s %s\n",
+				tz.Location().String(),
+				t.Location().String(),
+				t.Format(time.RFC3339))
+		}*/
+		if h := t.Hour(); h != 0 {
+			if h >= 12 {
+				h = h - 24
+				t = t.Add(time.Duration(-h) * time.Hour)
+			}
 		}
-		midnight = midnight.Add(time.Duration(-h) * time.Hour)
+		//}
 	}
+	return t
+}
 
-	return midnight
+func IsMidnight(t time.Time) bool {
+	return !t.IsZero() && t.Hour() == 0 && t.Minute() == 0 && t.Second() == 0
+}
+
+// YesterdayMidnight returns the time.Time for midnight of the previous day from t.
+// This accounts for any changes within t's TimeZone, e.g. Daylight Saving
+func YesterdayMidnight(t time.Time) time.Time {
+	return LocalMidnight(LocalMidnight(t).Add(-time.Hour))
+}
+
+// TomorrowMidnight returns the time.Time for midnight of the following day from t.
+// This accounts for any changes within t's TimeZone, e.g. Daylight Saving
+func TomorrowMidnight(t time.Time) time.Time {
+	midnight := LocalMidnight(t)
+	dayId := DayId(midnight)
+
+	// Check tomorrow allowing for days of 23, 24 or 25 hours in length
+	tomorrow := midnight
+	for h := 23; h < 26; h++ {
+		tomorrow = LocalMidnight(midnight.Add(time.Duration(h) * time.Hour))
+		if DayId(tomorrow) > dayId {
+			break
+		}
+	}
+	return tomorrow
+}
+
+// DayId returns a unique id for a specific date.
+// It's equivalent to (Year*366) + YearDay. 366 is used to allow for leap years,
+// so for non-leap years there will be a gap in the sequence between 31-Dec and 1-Jan.
+func DayId(t time.Time) int {
+	return (t.Year() * 366) + t.YearDay()
 }
--- util/time/util_test.go
+++ util/time/util_test.go
@@ -8,62 +8,160 @@ import (
 	"unicode"
 )
 
+func TestLocalMidnight2(t *testing.T) {
+	testTimeZone(t, "Europe/London", LocalMidnight, func(t *testing.T, timeZone string, tm, localTime, got time.Time) {
+		tmDayId := DayId(tm.In(localTime.Location()))
+		gotDayId := DayId(got)
+		if tmDayId != gotDayId {
+			t.Errorf("Not same day %d %d %s %s",
+				tmDayId, gotDayId,
+				tm.String(),
+				got.String(),
+			)
+		}
+	})
+}
+
 // For every timezone on the test machine, run local midnight against every hour
 // of the UTC year.
 //
 // This then tests things like Daylight Savings etc
 func TestLocalMidnight(t *testing.T) {
-	for _, timeZone := range getAvailableTimeZones() {
-		t.Run(timeZone, func(t *testing.T) {
-			loc, err := time.LoadLocation(timeZone)
-			if err != nil {
-				t.Errorf("Failed to load %q: %v", timeZone, err)
-				return
+	testAllTimeZones(t, LocalMidnight, func(t *testing.T, timeZone string, tm, localTime, got time.Time) {
+		// Check that midnight is on the same day as localTime.
+		// Note: tm.In() here is so that it handles DST
+		tmDayId := DayId(tm.In(got.Location()))
+		gotDayId := DayId(got)
+		if tmDayId != gotDayId {
+			t.Errorf("Not same day %d %d %s %s",
+				tmDayId, gotDayId,
+				tm.String(),
+				got.String(),
+			)
+		}
+	})
+}
+
+func TestYesterdayMidnight(t *testing.T) {
+	testAllTimeZones(t, YesterdayMidnight, func(t *testing.T, timeZone string, tm, localTime, got time.Time) {
+		// Now check the date is yesterday
+		midnight := LocalMidnight(localTime)
+		yesterday := LocalMidnight(midnight.Add(-time.Hour))
+
+		if !yesterday.Before(midnight) {
+			t.Errorf("%q is not yesterday for %q", got.String(), midnight.String())
+		}
+
+		// yesterday should match what the actual function have us
+		if !yesterday.Equal(got) {
+			t.Errorf("Not same date, %q yesterday %q got %q",
+				tm.String(),
+				got.String(),
+				yesterday.String())
+		}
+	})
+}
+
+func TestTomorrowMidnight(t *testing.T) {
+	testAllTimeZones(t, TomorrowMidnight, func(t *testing.T, timeZone string, tm, localTime, got time.Time) {
+		// Now check the date is tomorrow
+		midnight := LocalMidnight(localTime)
+		mYd := DayId(midnight)
+
+		// Lookup tomorrow, checking for days which are 23, 24 or 25 hours long
+		tomorrow := midnight
+		for i := 23; i < 26; i++ {
+			tomorrow = LocalMidnight(midnight.Add(time.Duration(i) * time.Hour))
+			if DayId(midnight) > mYd {
+				break
 			}
+		}
 
-			// test across a four-year period where one, 2024, is a leap year
-			for year := 2023; year <= 2026; year++ {
-				tm := time.Date(year, 1, 1, 0, 13, 14, 0, time.UTC)
+		if !tomorrow.After(localTime) {
+			t.Errorf("%q %q is not tomorrow for %q", tm.String(), got.String(), localTime.String())
+		}
 
-				for tm.Year() == year {
-					localTime := tm.In(loc)
+		// tomorrow should match what the actual function have us
+		if !tomorrow.Equal(got) {
+			t.Errorf("Not same date, %q tomorrow %q got %q",
+				tm.String(),
+				got.String(),
+				tomorrow.String())
+		}
+	})
+}
 
-					got := LocalMidnight(localTime)
+func testAllTimeZones(t *testing.T, f func(time.Time) time.Time, test func(t *testing.T, timeZone string, tm, localTime, got time.Time)) {
+	for _, timeZone := range getAvailableTimeZones() {
+		testTimeZone(t, timeZone, f, test)
+	}
+}
 
-					// We would expect midnight to occur at 00:00:00
-					hour := got.Hour()
-					failed := hour != 0
+func testTimeZone(t *testing.T, timeZone string, f func(time.Time) time.Time, test func(t *testing.T, timeZone string, tm, localTime, got time.Time)) {
+	t.Run(timeZone, func(t *testing.T) {
+		loc, err := time.LoadLocation(timeZone)
+		if err != nil {
+			t.Errorf("Failed to load %q: %v", timeZone, err)
+			return
+		}
 
-					// If it's not 0 then check special cases
-					if failed {
-						switch timeZone {
-						// These Time Zones switch to DST at midnight,
-						// so the start of this single local day is 01:00 and not 00:00
-						// e.g. 23:59:59 is followed by 01:00:00
-						case "Africa/Cairo",
-							"America/Asuncion",
-							"America/Havana",
-							"America/Santiago",
-							"America/Scoresbysund",
-							"Asia/Beirut",
-							"Atlantic/Azores",
-							"Chile/Continental",
-							"Cuba",
-							"Egypt":
-							failed = hour != 1
-						}
-					}
+		// test across a four-year period where one, 2024, is a leap year.
+		// Note: This time span is in UTC not the timezone being tested
+		for year := 2023; year <= 2026; year++ {
+			tm := time.Date(year, 1, 1, 0, 13, 14, 0, time.UTC)
 
-					if failed {
-						t.Errorf("%s got %s for %q", localTime.Format(time.RFC3339), got.Format(time.RFC3339), timeZone)
-					}
+			for tm.Year() == year {
+				localTime := tm.In(loc)
 
-					tm = tm.Add(time.Hour)
+				got := f(localTime)
+
+				testTimeIsMidnight(t, timeZone, localTime, got)
+
+				if test != nil {
+					test(t, timeZone, tm, localTime, got)
 				}
 
+				tm = tm.Add(time.Hour)
 			}
-		})
+
+		}
+	})
+}
+
+// testTimeIsMidnight tests that got is pointing to Midnight.
+// This accounts for some Time Zones where when DST occurs and there is no Midnight when the DST transition occurs.
+func testTimeIsMidnight(t *testing.T, timeZone string, localTime, got time.Time) {
+	// We would expect midnight to occur at 00:00:00
+	failed := !IsMidnight(got)
+
+	// If it's not 0 then check special cases
+	if failed {
+		switch timeZone {
+		// These Time Zones switch to DST at midnight,
+		// so the start of this single local day is 01:00 and not 00:00
+		// e.g. 23:59:59 is followed by 01:00:00
+		case "Africa/Cairo",
+			"America/Asuncion",
+			"America/Havana",
+			"America/Santiago",
+			"America/Scoresbysund",
+			"Asia/Beirut",
+			"Atlantic/Azores",
+			"Chile/Continental",
+			"Cuba",
+			"Egypt":
+			failed = got.Hour() != 1
+		}
 	}
+
+	//if !failed {
+	//	failed = !IsMidnight(got)
+	//}
+
+	if failed {
+		t.Errorf("%s got %s for %q", localTime.Format(time.RFC3339), got.Format(time.RFC3339), timeZone)
+	}
+
 }
 
 func getAvailableTimeZones() []string {
