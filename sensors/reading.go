c5da84eea39f8b16d495bcf6b4096daca1c9ccc0
peter <peter@retep.org>
2024-11-15 14:29:16 +0000

    Fix Reading so it marshall's correctly

--- sensors/reading.go
+++ sensors/reading.go
@@ -2,6 +2,7 @@ package sensors
 
 import (
 	"encoding/json"
+	"github.com/peter-mount/go-kernel/v2/log"
 	"github.com/peter-mount/piweather.center/weather/value"
 	"time"
 )
@@ -16,6 +17,8 @@ type Reading struct {
 
 	// Readings from the sensor
 	Readings map[string]value.Value
+
+	keys []string
 
 	// Device model
 	Device string
@@ -39,29 +42,39 @@ func (r *Reading) Set(n string, v value.Value) {
 	if r.Readings == nil {
 		r.Readings = make(map[string]value.Value)
 	}
+	if _, exists := r.Readings[n]; !exists {
+		r.keys = append(r.keys, n)
+	}
 	r.Readings[n] = v
+}
+
+func (r *Reading) SetInt(n string, unit *value.Unit, v int) {
+	r.SetFloat64(n, unit, float64(v))
+}
+
+func (r *Reading) SetFloat64(n string, unit *value.Unit, v float64) {
+	r.Set(n, unit.Value(v))
 }
 
 func (r *Reading) MarshalJSON() ([]byte, error) {
 	var buf []byte
 	buf = append(buf, `{"time":"`...)
 	buf = append(buf, r.Time.Format(time.RFC3339)...)
+	buf = append(buf, '"')
 
 	if r.ID != "" {
-		buf = append(buf, `","id":"`...)
+		buf = append(buf, `,"id":"`...)
 		buf = append(buf, r.ID...)
+		buf = append(buf, '"')
 	}
 
-	buf = append(buf, `","readings":`...)
-	if r.Readings == nil {
-		buf = append(buf, "null"...)
-	} else {
-		b1, err := json.Marshal(r.Readings)
-		if err != nil {
-			return nil, err
-		}
-		buf = append(buf, b1...)
-	}
+	buf = r.marshallMap(buf, "readings", func(v value.Value) string {
+		return v.PlainString()
+	})
+
+	buf = r.marshallMap(buf, "units", func(v value.Value) string {
+		return `"` + v.Unit().ID() + `"`
+	})
 
 	buf = append(buf, `,"device":"`...)
 	buf = append(buf, r.Device...)
@@ -70,6 +83,34 @@ func (r *Reading) MarshalJSON() ([]byte, error) {
 	return buf, nil
 }
 
+func (r *Reading) marshallMap(buf []byte, name string, f func(k value.Value) string) []byte {
+	buf = append(buf, `,"`...)
+	buf = append(buf, name...)
+	buf = append(buf, `":`...)
+
+	if r.Readings == nil {
+		return append(buf, "null"...)
+	}
+
+	buf = append(buf, '{')
+
+	sep := false
+	for _, k := range r.keys {
+		if v, exists := r.Readings[k]; exists {
+			if sep {
+				buf = append(buf, ',')
+			} else {
+				sep = true
+			}
+			buf = append(buf, '"')
+			buf = append(buf, k...)
+			buf = append(buf, '"', ':')
+			buf = append(buf, f(v)...)
+		}
+	}
+
+	return append(buf, '}')
+}
 func NewReading(dev Device) *Reading {
 	return &Reading{
 		Time:     time.Now().UTC(), // default to now but usually the device will override it
@@ -129,4 +170,33 @@ func (r *Reading) Differences(b *Reading) []Difference {
 	}
 
 	return ret
+}
+
+type Publisher func(*Reading) error
+
+func (a Publisher) Then(b Publisher) Publisher {
+	if a == nil {
+		return b
+	}
+	if b == nil {
+		return a
+	}
+	return func(r *Reading) error {
+		err := a(r)
+		if err == nil {
+			err = b(r)
+		}
+		return err
+	}
+}
+
+func LogPublisher(r *Reading) error {
+	if log.IsVerbose() {
+		b, err := json.Marshal(r)
+		if err != nil {
+			return err
+		}
+		log.Println(string(b))
+	}
+	return nil
 }
