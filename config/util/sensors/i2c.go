87441f739dedda6d986765129c9d7bd8d942fb9f
peter <peter@retep.org>
2024-11-20 14:18:26 +0000

    Fix i2c syntax

--- config/util/sensors/i2c.go
+++ config/util/sensors/i2c.go
@@ -4,6 +4,6 @@ import "github.com/alecthomas/participle/v2/lexer"
 
 type I2C struct {
 	Pos    lexer.Position
-	Bus    int `parser:"'i2c' '(' 'bus' @Number"`
-	Device int `parser:"'address' @Number ')'"`
+	Bus    int `parser:"'i2c' 'bus' @Int"`
+	Device int `parser:"'address' @Int"`
 }
