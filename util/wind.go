f12e28312c56b548f229f13d39efa2e11ea7da6b
peter <peter@retep.org>
2024-11-25 13:40:03 +0000

    Convert WindCompass to an enum

--- util/wind.go
+++ util/wind.go
@@ -1,6 +1,57 @@
 package util
 
-import "math"
+import (
+	"github.com/peter-mount/piweather.center/weather/measurement"
+	"github.com/peter-mount/piweather.center/weather/value"
+	"math"
+)
+
+type WindCompass uint8
+
+func (w WindCompass) String() string {
+	return windCompassDirection[w]
+}
+
+// Int returns the value as an index between 0..15
+func (w WindCompass) Int() int {
+	return int(w)
+}
+
+// Degree returns the WindCompass in degrees, N=0 increasing clockwise
+func (w WindCompass) Degree() float64 {
+	return float64(w) * 22.5
+}
+
+// Add returns WindCompass with an increment (0..15) added to it.
+// To subtract pass a negative value
+func (w WindCompass) Add(i int) WindCompass {
+	return WindCompass((int(w) + i) % 16)
+}
+
+// AddDegree returns a WindCompass with the appropriate number of degrees added.
+// Clockwise is positive, CounterClockwise is negative.
+func (w WindCompass) AddDegree(i float64) WindCompass {
+	return WindCompassDirection(w.Degree() + i)
+}
+
+const (
+	WindN = iota
+	WindNNE
+	WindNE
+	WindENE
+	WindE
+	WindESE
+	WindSE
+	WindSSE
+	WindS
+	WindSSW
+	WindSW
+	WindWSW
+	WindW
+	WindWNW
+	WindNW
+	WindNNW
+)
 
 var (
 	windCompassDirection = []string{
@@ -11,13 +62,24 @@ var (
 	}
 )
 
-func WindCompassDirection(d float64) string {
+func WindCompassDirection(d float64) WindCompass {
 	for d < 0 {
 		d += 360
 	}
 	for d >= 360 {
 		d -= 360
 	}
-	e := int(math.Floor((d+11.25)/22.5)) % 16
-	return windCompassDirection[e]
+	return WindCompass(int(math.Floor((d+11.25)/22.5)) % 16)
+}
+
+func WindCompassDirectionDegrees(d value.Value) (WindCompass, error) {
+	v, err := d.As(measurement.Degree)
+	if err == nil {
+		err = v.BoundsError()
+	}
+	if err != nil {
+		return 0, err
+	}
+
+	return WindCompassDirection(v.Float()), nil
 }
