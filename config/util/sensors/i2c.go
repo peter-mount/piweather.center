7eff5660f0b70aadeaee9c52b043655503a5f439
peter <peter@retep.org>
2024-12-03 18:05:19 +0000

    Remove old sensors config

--- config/util/sensors/i2c.go
+++
@@ -1,9 +0,0 @@
-package sensors
-
-import "github.com/alecthomas/participle/v2/lexer"
-
-type I2C struct {
-	Pos    lexer.Position
-	Bus    int `parser:"'i2c' 'bus' @Int"`
-	Device int `parser:"'address' @Int"`
-}
