41e2e0bea12c28e7921c2672a48e4996b6b3c038
peter <peter@retep.org>
2024-12-17 07:55:39 +0000

    Fix border dimensions

--- astro/chart/helpers.go
+++ astro/chart/helpers.go
@@ -6,14 +6,30 @@ import (
 
 // FloodFillLayer is a Layer which will fill the entire chart with the Fill colour.
 func FloodFillLayer(proj Projection) ConfigurableLayer {
+	return borderLayer(proj, func(gc draw2d.GraphicContext) {
+		gc.Fill()
+	})
+}
+
+// BorderLayer is a Layer which will draw a border around the entire chart with the Stroke colour.
+func BorderLayer(proj Projection) ConfigurableLayer {
+	return borderLayer(proj, func(gc draw2d.GraphicContext) {
+		gc.Stroke()
+	})
+}
+
+// borderLayer used by FloodFillLayer and BorderLayer
+func borderLayer(proj Projection, drawable Drawable) ConfigurableLayer {
 	return NewDrawableLayer(func(gc draw2d.GraphicContext) {
 		b := proj.Bounds()
+		x0, y0 := -1.0, -1.0
+		x1, y1 := float64(b.Dx()), float64(b.Dy())
 		gc.BeginPath()
-		gc.MoveTo(0, 0)
-		gc.LineTo(float64(b.Dx()-1), 0)
-		gc.LineTo(float64(b.Dx()-1), float64(b.Dy()-1))
-		gc.LineTo(0, float64(b.Dy()-1))
+		gc.MoveTo(x0, y0)
+		gc.LineTo(x1, y0)
+		gc.LineTo(x1, y1)
+		gc.LineTo(x0, y1)
 		gc.Close()
-		gc.FillStroke()
+		drawable(gc)
 	})
 }
