c29ef702dbf49ece0864947fb605dc57a5a5975c
peter <peter@retep.org>
2024-12-19 16:03:19 +0000

    switch to new layout engine

--- demos/timelapse/skycamframe.c
+++ demos/timelapse/skycamframe.c
@@ -102,26 +102,25 @@ main() {
 createLayout(cfg) {
     cfg.layout = layout.New(image.Width4K,image.Height4K).
         RowContainer().
-            ColScaleContainer(0.5,0.5).
+            ColScaleContainer(1/3.0,1/3.0,1/3.0).
                 Font("luxi 32 mono bold").
                 Fill( cfg.white ).
                 Text("",cfg.title).End().
+                Text("","ME15Weather").Align("center").End().
                 Text("timeDisplay","%s").Align("right").End().
-                End().
+            End().
             ColScaleContainer(0.4,0.4,0.2).
                 Image("skyCamera").Inset(10).End().
                 Image("auxView").Inset(10).End().
-                End().
-            ColScaleContainer(1.0).
-                Font("luxi 20 mono bold").
-                Fill( cfg.white ).
                 RowContainer().
+                    Font("luxi 20 mono bold").
+                    Fill( cfg.white ).
                     Text("cloudCover", "Cloud Cover %3.0f%% Sky %3.0f%% Obscured %3.0f%%").End().
-                    Text("", "JabberGhjkfjdf").End().
-                    Text("", "JabberGhjkfjdf").End().
-                    End().
+                    Text("jab1", "JabberGhjkfjdf").End().
+                    Text("jab2", "JabberGhjkfjdf").End().
                 End().
             End().
         End().
-        Build()
+    End().
+    Build()
 }
